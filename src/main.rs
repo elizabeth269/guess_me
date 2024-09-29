e570e3c31a88b125d77b92e12a0e47a4ca5b37fd
Elizbeth <elizabethadebayo269@gmail.com>
2024-09-29 22:47:41 +0100

    comparing logic done

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
-
 fn main() {
-    println!("Welcome to the guessing game, enter a number from 1 to 100");
-    //get user input
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("coudn't process your guess");
-    println!("you guessed: {}", guess);
-
     //generate random number
-    // let random_number = rand::thread_rng().gen_range(1..=100);
-    println!("Random number is {}", rand::thread_rng().gen_range(1..=100))
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    // println!("Random number is {}", random_number);
+    println!("Welcome to the guessing game, enter a number from 1 to 100");
+    loop {
+        //get user input
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("coudn't process your guess");
+
+        //converting our guess from string to an integer
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // expect("not an integer");
+        println!("you guessed: {}", guess);
+
+        //assert or check if the random numbers are equal
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
 }
 
 //get user input
