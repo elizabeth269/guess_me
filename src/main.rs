92adca750a73cfa502dcdd51af0a73e5526c8c27
Elizbeth <elizabethadebayo269@gmail.com>
2024-09-29 22:19:04 +0100

    user input logic is done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game, enter a number from 1 to 100");
+    //get user input
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("coudn't process your guess");
+    println!("you guessed: {}", guess)
 }
+
+//get user input
+//generate random number
+//assert or check if the random numbers are equal
